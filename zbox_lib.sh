8c7520f58476bdcb4fade78973f6dee6317e78fe
stico <ouyzhu@gmail.com>
2014-01-10 16:15:49 +0800

    update shell lib functions

--- zbox_lib.sh
+++ zbox_lib.sh
@@ -2,11 +2,11 @@
 
 # source ${HOME}/.myenv/myenv_lib.sh || eval "$(wget -q -O - "https://raw.github.com/stico/myenv/master/.myenv/myenv_lib.sh")" || exit 1
 
-function func_date {	date "+%Y-%m-%d";		}
-function func_time {	date "+%H-%M-%S";		}
-function func_dati {	date "+%Y-%m-%d_%H-%M-%S";	}
+function func_date() {	date "+%Y-%m-%d";		}
+function func_time() {	date "+%H-%M-%S";		}
+function func_dati() {	date "+%Y-%m-%d_%H-%M-%S";	}
 
-function func_die {
+function func_die() {
 	[ $# -lt 1 ] && echo -e "Usage: $FUNCNAME [error_info] \n Desc: echo error info to stderr and exit \n" && exit 1
 	
 	echo -e "$@" 1>&2
